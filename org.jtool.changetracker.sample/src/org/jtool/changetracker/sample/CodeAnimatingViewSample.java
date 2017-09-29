b1d7f8078350fff68906ca069460f5bf856db657
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2017-09-29 21:34:18 +0900

    created sample

---
+++ org.jtool.changetracker.sample/src/org/jtool/changetracker/sample/CodeAnimatingViewSample.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2017
+ *  Software Science and Technology Lab.
+ *  Department of Computer Science, Ritsumeikan University
+ */
+
+package org.jtool.changetracker.sample;
+
+import org.jtool.changetracker.replayer.ui.CodeAnimatingView;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * A view that shows the results of comparing between two contents of code.
+ * @author Katsuhisa Maruyama
+ */
+public class CodeAnimatingViewSample extends CodeAnimatingView {
+    
+    /**
+     * The identification string that is used to register this view.
+     */
+    public static final String ID = "org.jtool.changetracker.sample.CodeAnimatingViewSample";
+    
+    /**
+     * Creates a code view.
+     */
+    public CodeAnimatingViewSample() {
+        super();
+    }
+    
+    /**
+     * Creates a code viewer.
+     * @return the control for the created code viewer
+     */
+    @Override
+    protected Control createCodeView(Composite parent) {
+        Control control = super.createCodeView(parent);
+        
+        MenuManager manager = getMenuManager();
+        manager.addMenuListener(new IMenuListener() {
+            
+            /**
+             * Receives the event when the menu is about to be shown by the given menu manager.
+             * @param manager the menu manager
+             */
+            @Override
+            public void menuAboutToShow(IMenuManager manager) {
+                manager.add(new LabelAction("Label"));
+                manager.add(new TextSelectionAction("Select"));
+            }
+        });
+        return control;
+    }
+    
+    /**
+     * An action for selecting text.
+     */
+    private class TextSelectionAction extends Action {
+        
+        /**
+         * Creates an instance for performing the backward slicing.
+         * @param text the text displayed as a menu item
+         */
+        public TextSelectionAction(String text) {
+            super();
+            
+            setText(text);
+            setEnabled(true);
+        }
+        
+        /**
+         * Performs the menu action.
+         */
+        @Override
+        public void run() {
+            TextSelection selection = getSelection();
+            if (selection != null && selection.getLength() > 0) {
+                System.out.println(selection.getText());
+            }
+        }
+    }
+}
---
+++ org.jtool.changetracker.sample/src/org/jtool/changetracker/sample/OperationReplayerSample.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2017
+ *  Software Science and Technology Lab.
+ *  Department of Computer Science, Ritsumeikan University
+ */
+
+package org.jtool.changetracker.sample;
+
+import org.jtool.changetracker.replayer.ui.HistoryView;
+import org.jtool.changetracker.replayer.ui.OperationVisualizer;
+import org.jtool.changetracker.repository.IRepositoryHandler;
+
+/**
+ * Replays operation changes.
+ * @author Katsuhisa Maruyama
+ */
+public class OperationReplayerSample extends OperationVisualizer implements IRepositoryHandler {
+    
+    
+    /**
+     * Creates an object that replays change operations.
+     */
+    public OperationReplayerSample() {
+    }
+    
+    /**
+     * Invoked to initialize this handler before receiving repository change events.
+     */
+    public void initialize() {
+        addView(HistoryView.ID);
+        // addView(CodeComparingViewSample.ID);
+        // addView(CodeAnimatingViewSample.ID);
+    }
+    
+    /**
+     * Invoked to terminate this handler.
+     */
+    public void terminate() {
+    }
+}
